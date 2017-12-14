3de811cdcfba4ac4f2cffb9d595ec66acd0e68da
Finn <finn.potason@gmail.com>
2017-12-15 00:49:29 +0100

    outline working

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var hbs = require('express-handlebars');// Finn
 
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var members = require('./routes/members');
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+// app.use('/members', members);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+    res.render('index', { condition: true, anyArray: [1,2,3] });
 });
+
+
+router.get('/about.html', function(req, res, next) {
+    res.render('about', { title: 'About', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/members.html', function(req, res, next) {
+    res.render('members', { title: 'Members', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/projects.html', function(req, res, next) {
+    res.render('projects', { title: 'Projects', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/publications.html', function(req, res, next) {
+    res.render('publications', { title: 'Publications', condition: true, anyArray: [1,2,3] });
+});
+
 
 module.exports = router;
