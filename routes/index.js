be941447589caaa8dcdb74157ea21848ec4b1e75
Finn <finn.potason@gmail.com>
2017-12-15 02:28:30 +0100

    converstaion

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,44 @@
 var express = require('express');
 var router = express.Router();
 
+
+// var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+var watson = require("watson-developer-cloud");
+
+var conversation = new watson.ConversationV1 ({
+    // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
+    // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
+    'username': '83490051-78f4-4ef0-b96f-4760d94c7050',
+    'password': 'Di0IxL6GHSr1',
+    'version_date': '2017-11-26'
+});
+
+
+// conversation.listWorkspaces(function(err, response) {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log(JSON.stringify(response, null, 2));
+//     }
+// });
+
+
+// conversation.message({
+//     workspace_id: '49317eb4-0292-4f3d-a6ea-93dc547151ac',
+//     input: {'text': 'Hello'}
+// },  function(err, response) {
+//     if (err)
+//         console.log('error:', err);
+//     else
+//         // console.log(JSON.stringify(response, null, 2));
+//         console.log(response.output.text);
+// });
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { condition: true, anyArray: [1,2,3] });
@@ -13,7 +51,26 @@ router.get('/about.html', function(req, res, next) {
 });
 
 router.get('/members.html', function(req, res, next) {
-    res.render('members', { title: 'Members', condition: true, anyArray: [1,2,3] });
+
+    var chatbot_reply; // empty string for now
+
+    conversation.message({
+    workspace_id: '49317eb4-0292-4f3d-a6ea-93dc547151ac',
+    input: {'text': 'Hello'}
+},  function(err, response) {
+    if (err)
+        console.log('error:', err);
+    else
+        console.log(JSON.stringify(response, null, 2));
+        // console.log(response.output.text);
+        chatbot_reply = response.output.text
+        console.log(chatbot_reply)
+        res.render('members', { title: 'Members', condition: true, anyArray: [1,2,3], reply: chatbot_reply });
+    });
+
+
+    // res.render('members', { title: 'Members', condition: true, anyArray: [1,2,3], reply: chatbot_reply });
+
 });
 
 router.get('/projects.html', function(req, res, next) {
